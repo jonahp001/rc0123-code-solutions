81cfb032c946751db231e757f84134040f588620
Jonah Park <jonahp1234@gmail.com>
2023-01-30 19:23:41 +0000

    Jonah Park javascript-operators-and-expressions Exercise

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 4;
+var height = 6;
+var area = width * height;
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 45;
+var payment = 80;
+var change = payment - bill;
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 49;
+var midterm = 92;
+var final = 84;
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Jonah';
+var lastName = 'Park';
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 4;
+var isAcidic = pH < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount = 89;
+var isSparta = headCount === 300;
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName;
+motto += ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);

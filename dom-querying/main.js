054da2fb2c3b838e6b909075a1b047d15cf3e2a7
Jonah Park <jonahp1234@gmail.com>
2023-02-06 17:51:10 +0000

    Jonah Park dom-querying Exercise

---
+++ dom-querying/main.js
@@ -0,0 +1,19 @@
+console.log('hello, world');
+
+var h1Element = document.querySelector('h1');
+console.log(h1Element);
+console.dir();
+
+var explanationElement = document.querySelector('explanation');
+console.log(explanationElement);
+console.dir();
+
+var hintElement = document.querySelector('hint');
+console.log(hintElement);
+console.dir();
+
+var pElement = document.querySelectorAll('p');
+console.log(pElement);
+
+var exampleLinkNodeList = document.querySelectorAll('example-link');
+console.log(exampleLinkNodeList);

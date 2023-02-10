80ce859efaabf655ad81f274673239631533f644
Jonah Park <jonahp1234@gmail.com>
2023-02-10 00:02:16 +0000

    Jonah Park javascript-view-swapping Exercise

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,26 @@
+var $tabContainer = document.querySelector('.tab-container');
+var $allTabs = document.querySelectorAll('.tab');
+var $allViews = document.querySelectorAll('.view');
+
+$tabContainer.addEventListener('click', function (event) {
+  if (event.target.matches('.tab')) {
+    for (var i = 0; i < $allTabs.length; i++) {
+      if ($allTabs[i] === event.target) {
+        $allTabs[i].className = 'tab active';
+      } else {
+        $allTabs[i].className = 'tab';
+      }
+    }
+  }
+  var $csLanguage = event.target.getAttribute('data-view');
+  for (var j = 0; j < $allViews.length; j++) {
+    if ($allViews[j].getAttribute('data-view') !== $csLanguage) {
+      // $csLanguage.className = 'view';
+      $allViews[j].className = 'view hidden';
+    } else {
+      $allViews[j].className = 'view';
+      // $csLanguage.className = 'view';
+    }
+  }
+
+});

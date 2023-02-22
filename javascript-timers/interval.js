605ab499ec55eb54876d8f7df8bdedf6c7e74b3f
Jonah Park <jonahp1234@gmail.com>
2023-02-22 17:44:01 +0000

    Jonah Park javascript-timers Exercise

---
+++ javascript-timers/interval.js
@@ -0,0 +1,15 @@
+var $h1 = document.querySelector('h1');
+
+function countDownEarth() {
+  if ($h1.textContent === '4') {
+    $h1.textContent = '3';
+  } else if ($h1.textContent === '3') {
+    $h1.textContent = '2';
+  } else if ($h1.textContent === '2') {
+    $h1.textContent = '1';
+  } else if ($h1.textContent === '1') {
+    $h1.textContent = '~Earth Beeeelooowww Us~';
+  }
+}
+
+setInterval(countDownEarth, 1000);

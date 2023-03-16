bd2ec15b753a24f0ce71f37acc899d5e160dabe2
Jonah Park <jonahp1234@gmail.com>
2023-03-16 16:32:50 +0000

    very slight adjustments

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -3,7 +3,7 @@ import { readFile } from 'node:fs/promises';
 for (let i = 2; i < process.argv.length; i++) {
   try {
     const filePath = new URL(process.argv[i], import.meta.url);
-    const contents = await readFile(process.argv[i], { encoding: 'utf8' });
+    const contents = await readFile(filePath, { encoding: 'utf8' });
     console.log(contents);
   } catch (err) {
     console.error(err.message);

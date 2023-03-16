ae3072b2cb74d6242b2e81894e1ace4846fcfb36
Jonah Park <jonahp1234@gmail.com>
2023-03-16 04:59:59 +0000

    Jonah Park node-fs-readfile Exercise

---
+++ node-fs-readfile/cat.js
@@ -0,0 +1,11 @@
+import { readFile } from 'node:fs/promises';
+
+for (let i = 2; i < process.argv.length; i++) {
+  try {
+    const filePath = new URL(process.argv[i], import.meta.url);
+    const contents = await readFile(process.argv[i], { encoding: 'utf8' });
+    console.log(contents);
+  } catch (err) {
+    console.error(err.message);
+  }
+}

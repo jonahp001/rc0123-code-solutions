23d461b97066ab69a86797660eda8fceb990abb0
Jonah Park <jonahp1234@gmail.com>
2023-03-16 16:45:41 +0000

    very very slight adjustments

--- node-fs-readfile/read-dijkstra-promise.js
+++ node-fs-readfile/read-dijkstra-promise.js
@@ -1,7 +1,7 @@
 import { readFile } from 'node:fs/promises';
 
 try {
-  const filePath = new URL('dijkstra.txt', import.meta.url);
+  const filePath = new URL('./dijkstra.txt', import.meta.url);
   const contents = await readFile(filePath, { encoding: 'utf8' });
   console.log(contents);
 } catch (err) {
